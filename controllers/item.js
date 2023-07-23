a672c9695f44f72d2e26434324b0272ce88d22c3
Rhoenzfe <rhoenz.fermin@gmail.com>
2023-07-22 23:22:15 -0400

    able to login with existing user in database

--- controllers/item.js
+++ controllers/item.js
@@ -1,24 +1,45 @@
+const jwt = require('jsonwebtoken')
 const config = require ('../utils/config')
 const express = require('express')
 const Item = require('../models/item')
 const itemsRouter = express.Router()
 
-itemsRouter.get('/', (request, response) => {
-  Item
+itemsRouter.get('/', async (request, response) => {
+  const items = await Item
     .find({})
-    .then(items => {
-      response.json(items)
-    })
+    .populate("user", {username: 1, fistName: 1, lastName: 1})
+  response.json(items)
 })
 
-itemsRouter.post('/', (request, response) => {
-  const item = new Item(request.body)
+itemsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const token = request.token
+  const decodedToken = jwt.verify(token, config.SECRET)
+  if (!decodedToken.id) {
+    return response
+      .status(401)
+      .json({ error: 'Token missing or invalid' })
+  }
 
-  item
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const user = request.user
+  const item = await new Item({
+    itemName: body.itemName,
+    category: body.category,
+    description: body.description,
+    startingBid: body.startingBid,
+    startTime: body.startTime,
+    startDate: body.startDate,
+    endDate: body.endDate,
+    user: user._id
+  }).populate("user", {username: 1, firstName: 1, lastName: 1})
+
+  const save = await item.save()
+  user.items = user.items.concat(save._id)
+  await user.save()
+  
+  response
+  .status(201)
+  .json(save.toJSON())
 })
 
 module.exports = itemsRouter
