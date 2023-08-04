565f52c5ce8e89adf5b77a8dedb00ad60d00fd88
Syed Mukkerim <syedmukkerrim@gmail.com>
2023-08-04 13:56:01 -0400

    Revert "Merging Code"
    This reverts commit b03abdc7c1f6c1687dcae11a02f0c0f64939d0ad.

--- routes/auctionRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createAuction } = require("../controller/auctionController");
-const { protect } = require("../controller/authController");
-
-router.post("/create", protect, createAuction);
-
-module.exports = router;
