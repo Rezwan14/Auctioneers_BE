30b4172aaff403a881b0ef40b312ab299308c98a
Rhoenzfe <102177713+rhoenz-fe@users.noreply.github.com>
2023-08-15 20:18:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const config = require("./utils/config")
+const config = require("./utils/config") //config
 const express = require("express")
 require("express-async-errors")
 const app = express()
 const cors = require("cors")
-const logger = require("./utils/logger")
-const middleware = require("./utils/middleware")
-const mongoose = require("mongoose")
-const itemsRouter = require('./controllers/item')
-const usersRouter = require("./controllers/user")
-const loginRouter = require("./controllers/login")
+const logger = require("./utils/logger") //logger
+const middleware = require("./utils/middleware") //middleware
+const mongoose = require("mongoose") //mongoDB
+const itemsRouter = require('./controllers/item') //router for items
+const usersRouter = require("./controllers/user") //router for users
+const loginRouter = require("./controllers/login") //router for login
 const url = config.MONGODB_URI
-logger.info("connecting to MongoDB")
 
+logger.info("connecting to MongoDB")//log mongoDB connection attemp
+
+//connec to mongoDB
 mongoose
   .connect(url)
   .then(() => {
@@ -21,12 +23,13 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.use(middleware.requestLogger) //log incoming routes
+app.use(middleware.tokenExtractor) //extract token from request
+
+//route handling
 app.use('/api/items', middleware.userExtractor, itemsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/login", loginRouter)
-
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
